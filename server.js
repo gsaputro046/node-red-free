004ebb0489f0afe4216c3d39ea43747d37d6e8af
gsaputro046 <gsaputro046@gmail.com>
2025-11-23 10:49:16 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const RED = require("node-red");
+
+const app = express();
+const server = require("http").createServer(app);
+
+const settings = {
+    httpAdminRoot: "/",
+    httpNodeRoot: "/api",
+    userDir: "./",
+    flowFile: "flows.json",
+    functionGlobalContext: {}
+};
+
+RED.init(server, settings);
+
+app.use(settings.httpAdminRoot, RED.httpAdmin);
+app.use(settings.httpNodeRoot, RED.httpNode);
+
+server.listen(process.env.PORT || 3000);
+
+RED.start();
